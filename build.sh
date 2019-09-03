54b7165da3d20adfdc8d9cc14165253eef95692a
xiaolu.dai <daixl@glodon.com>
2019-09-03 21:04:35 +0800

    update build

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 mvn clean package -DskipTests
-cp target/demo-0.0.1-SNAPSHOT.jar app.jar
 
 git co jar
+
+rm app.jar
+
+cp target/demo-0.0.1-SNAPSHOT.jar app.jar
 git add app.jar
 git cm 'deploy'
-
 
 git push
 
